741d70615ce518b7715668cd70df090218a4c246
Thiago Mota <th.araujomota@gmail.com>
2023-02-07 17:17:21 -0300

    feat: adds service tests and adds mocks

--- tests/Mocks/CarMock.ts
+++ tests/Mocks/CarMock.ts
@@ -4,6 +4,7 @@ export const validCar: ICar = {
   model: 'Uno da Escada',
   year: 1960,
   color: 'Red',
+  status: true,
   buyValue: 1500,
   doorsQty: 2,
   seatsQty: 2,
@@ -11,13 +12,13 @@ export const validCar: ICar = {
 
 export const validCarWithId: ICar = {
   id: '63e28260030bbd2db138c508',
-  model: 'Marea',
-  year: 2002,
-  color: 'Black',
+  model: 'Uno da Escada',
+  year: 1960,
+  color: 'Red',
   status: true,
-  buyValue: 15.990,
-  doorsQty: 4,
-  seatsQty: 5,
+  buyValue: 1500,
+  doorsQty: 2,
+  seatsQty: 2,
 };
 
 export const carsArray: ICar[] = [
@@ -36,6 +37,7 @@ export const carsArray: ICar[] = [
     model: 'Tempra',
     year: 1995,
     color: 'Black',
+    status: true,
     buyValue: 39.000,
     doorsQty: 2,
     seatsQty: 5,
---
+++ tests/unit/Services/carService.test.ts
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import CarService from '../../../src/Services/carService';
+import { carsArray, validCarWithId, validCar } from '../../Mocks/CarMock';
+
+describe('Testando a camada Service de Cars', function () {
+  it('Deve listar todos os carros registrados', async function () {
+    sinon.stub(Model, 'find').resolves(carsArray);
+
+    const findAll = new CarService();
+    const result = await findAll.findAll();
+
+    expect(result).to.be.deep.equal(carsArray);
+  });
+
+  it('Deve listar um carro específico', async function () {
+    sinon.stub(Model, 'findById').resolves(validCarWithId);
+
+    const findById = new CarService();
+    const result = await findById.findById('63e28260030bbd2db138c508');
+
+    expect(result).to.be.deep.equal(validCarWithId);
+  });
+
+  it('Deve registrar um novo carro', async function () {
+    sinon.stub(Model, 'create').resolves(validCarWithId);
+
+    const addCar = new CarService();
+    const result = await addCar.addsNewCar(validCar);
+
+    expect(result).to.be.deep.equal(validCarWithId);
+  });
+
+  it('Deve atualizar um carro específico', async function () {
+    sinon.stub(Model, 'findOne').resolves(validCarWithId);
+
+    const updateCar = new CarService();
+    const result = await updateCar.updateCar('63e28260030bbd2db138c508', validCar);
+    //  TypeError: updatedCar.save is not a function
+
+    expect(result).to.be.deep.equal(validCarWithId);
+  });
+});
