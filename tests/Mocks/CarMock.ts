465c2f2750e378fa2ca6e88daf1821aa693ab7a5
Thiago Mota <th.araujomota@gmail.com>
2023-02-07 16:49:00 -0300

    feat: adds mocks for testing

---
+++ tests/Mocks/CarMock.ts
@@ -0,0 +1,43 @@
+import ICar from '../../src/Interfaces/ICar';
+
+export const validCar: ICar = {
+  model: 'Uno da Escada',
+  year: 1960,
+  color: 'Red',
+  buyValue: 1500,
+  doorsQty: 2,
+  seatsQty: 2,
+};
+
+export const validCarWithId: ICar = {
+  id: '63e28260030bbd2db138c508',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const carsArray: ICar[] = [
+  {
+    id: '63e28260030bbd2db138c508',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '63e28260030bbd2db138c50a',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    buyValue: 39.000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
