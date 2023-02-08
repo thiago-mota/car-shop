6bc5d794d128d9f42df13e1f098d57f1cf86be26
Thiago Mota <th.araujomota@gmail.com>
2023-02-08 00:06:23 -0300

    feat: adds motorcycleController

---
+++ src/Controllers/motorcycleController.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleService from '../Services/motorcycleService';
+
+class MotorcycleController {
+  private request: Request;
+  private response: Response;
+  private next: NextFunction;
+  private service: MotorcycleService;
+
+  constructor(request: Request, response: Response, next: NextFunction) {
+    this.request = request;
+    this.response = response;
+    this.next = next;
+    this.service = new MotorcycleService();
+  }
+
+  public async create() {
+    const moto: IMotorcycle = {
+      model: this.request.body.model,
+      year: this.request.body.year,
+      color: this.request.body.color,
+      status: this.request.body.status || false,
+      buyValue: this.request.body.buyValue,
+      category: this.request.body.category,
+      engineCapacity: this.request.body.engineCapacity,
+    };
+
+    const newMoto = await this.service.addsNewMoto(moto);
+    return this.response
+      .status(201)
+      .json(newMoto);
+  }
+}
+
+export default MotorcycleController;
